36bec9feb4a11be58575650f2b58b3d2c42de796
Matthew Polak <polak.matthew2003@gmail.com>
2024-03-05 14:48:12 -0600

    First Upload

---
+++ nursingapp/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+        <title>Welcome Page</title>
+
+        <div style={{ backgroundColor: 'red', color: 'white'}}>
+          <h1>My Red Header</h1>
+        </div>
+        <h1>
+          Nursing Documentation Management Portal
+        </h1>
+    </div>
+  );
+}

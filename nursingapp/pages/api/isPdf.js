9713ffa7ce3a4b046a6a5c02bf33087858efc2c2
Harsha <harshasuresh0815@gmail.com>
2024-05-02 22:27:26 -0500

    comments

--- nursingapp/pages/api/isPdf.js
+++ nursingapp/pages/api/isPdf.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
         const userId = parseInt(userIdString, 10);
         
         try {
+            // Retrieves the user along with specified PDF fields from the database
             const pdfs = await prisma.user.findUnique({
                 where: {
                     id: userId,
